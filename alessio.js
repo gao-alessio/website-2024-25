c6d152965aa3b0043b60454194f75b76acf6ba59
gao-alessio <alessio.gao@myenaip.eu>
2024-12-12 09:50:59 +0000

    aggiornale

---
+++ alessio.js
@@ -0,0 +1,24 @@
+
+
+
+
+{
+   var pincoPallavar = 67;//obsoleto
+}
+
+{
+    let pincoPallalet = 34;// visibilità solo all'interno del codice
+    console.log("questa è pincoPallaVar:" + pincoPallaVar);
+}
+
+    console.log("questa è pincoPallaLet:" + pincoPallaLet);
+
+let esempio1 = 789;
+
+console.log(esempio1);
+esempio1 = "castagne";
+
+
+
+
+
