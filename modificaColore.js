79be4556f976c3d4fd5797e3cdc562131572078c
gao-alessio <alessio.gao@myenaip.eu>
2025-01-30 12:58:22 +0000

    aggiornare

--- modificaColore.js
+++ modificaColore.js
@@ -1,11 +1,21 @@
-<h2> Modifica il colore </h2>
-<div id ></div>
+
 
 const box = document.getElementById('box');
 const pulsante = document.getElementById('cambiaColorebtn');
 
 function coloreCasuale(){
-const lettere = '0123456789abcdf';
-const colore = '#';
+    const lettere = '0123456789ABCDEF';
+    const colore = '#';
+
+    for(let i = 0; i < 6; i++){
+    colore += lettere[floor(Math.random() * 16)];
+    }
+
+    return colore;
 
 }
+
+pulsante.addEventListener('click', function(){
+    box.style.backgroundColor = coloreCasuale();
+
+});
