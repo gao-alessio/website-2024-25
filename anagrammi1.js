198827aff21adcb3730faf563e060ed40270fdae
gao-alessio <alessio.gao@myenaip.eu>
2025-02-13 12:58:40 +0000

    aggiornale

---
+++ anagrammi1.js
@@ -0,0 +1,11 @@
+function sonoAnagrammi(parola1,parola2){
+    let palora1Clean = parola1.replace(/\s+/g, '').toLowerCase();
+    let palora2Clean = parola2.replace(/\s+/g, '').toLowerCase();
+console.log(palora1Clean,palora2Clean);
+}
+
+
+p1 ="cia u";
+p2 ="Bell o"
+
+sonoAnagrammi(p1,p2);
